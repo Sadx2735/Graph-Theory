dce8ebb4ff7adffb514cd338e718a6ac1398dffa
sadhasivam <Sadhasivam.Saravanan@trumpf.com>
2026-09-07 12:40:22 +0530

    v1.4 GraphRep

---
+++ Graph-Rep/CRep.cs
@@ -0,0 +1,36 @@
+﻿namespace Graphx;
+
+class CRepresenation {
+   static void RunMe (string[] args) {
+      string[] cities = new string[] { "bombay", "chennai", "kolkata", "cochin", "calicut" };
+      ComplexGraph Graphee = new (cities);
+      Graphee.AddEdge ("bombay", "chennai", true);
+      Graphee.AddEdge ("bombay", "kolkata", true);
+      Graphee.AddEdge ("bombay", "calicut", true);
+      Graphee.AddEdge ("cochin", "chennai", true);
+      Graphee.PrintAdjList ();
+   }
+}
+class ComplexGraph {
+   int N;
+   Dictionary<string, List<string>> Map;
+   public ComplexGraph (string[] cities) {
+      this.N = cities.Length;
+      this.Map = new Dictionary<string, List<string>> ();
+      for (int idx = 0; idx < N; idx++)
+         Map[cities[idx]] = new List<string> ();
+   }
+   public void AddEdge (string from, string to, bool UnDir = true) {
+      Map[from].Add (to);
+      if (UnDir) Map[to].Add (from);
+   }
+   public void PrintAdjList () {
+      foreach (var key in Map.Keys) {
+         Console.Write ($"{key} => ");
+         foreach (var val in Map[key]) {
+            Console.Write ($"{val},");
+         }
+         Console.WriteLine ();
+      }
+   }
+}
--- Graph-Rep/GraphCenter.cs
+++ Graph-Rep/GraphCenter.cs
@@ -13,4 +13,27 @@ class Graphify {
          return -1;
       }
    }
+
+   static void Runmex2 () {
+      int n = 5;
+      List<(int, int)> roads = [(0, 1), (0, 3), (1, 2), (1, 3), (2, 3), (2, 4)];
+      int[,] Map = new int[n, n];
+      Dictionary<int, int> Freq = [];
+      foreach (var item in roads) {
+         int u = item.Item1, v = item.Item2;
+         Map[u, v] = 1; Map[v, u] = 1;
+         if (Freq.ContainsKey (u)) Freq[u]++;
+         else Freq[u] = 1;
+         if (Freq.ContainsKey (v)) Freq[v]++;
+         else Freq[v] = 1;
+      }
+      int Maximum = 1 << 31;
+      for (int i = 0; i < n; i++) {
+         for (int j = 0; j < n; j++) {
+            if (i == j) continue;
+            Maximum = Math.Max (Maximum, Freq[i] + Freq[j] - Map[i, j]);
+         }
+      }
+      Console.WriteLine (Maximum);
+   }
 }
