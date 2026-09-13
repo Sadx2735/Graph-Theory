f0bc2b20b4b082eb755e355ccca0b117b4b246ca
Sadhasivam <Sadhasivam.Saravanan@trumpf.com>
2026-09-13 20:08:24 +0530

    Added BellMan Floyd Algo & One Dijkstra based LC prob

---
+++ ShortestPath/BellmanFord.cs
@@ -0,0 +1,37 @@
+﻿public class SolutionxBF {
+   public List<int> bellmanFord (int V, int[,] edges, int src) {
+      // Initializations for the algo
+      int[] dist = new int[V];
+      Array.Fill (dist, Int32.MaxValue);
+      dist[src] = 0;
+
+      int numEdges = edges.GetLength (0); 
+
+      // Run for N-1 and do the relaxation.
+      for (int i = 0; i < V - 1; i++) {
+         for (int j = 0; j < numEdges; j++) {
+            int from = edges[j, 0];
+            int to = edges[j, 1];
+            int weight = edges[j, 2];
+            // if we get to see a better shorter distance.
+            if (dist[from] != Int32.MaxValue && dist[from] + weight < dist[to]) {
+               dist[to] = dist[from] + weight;
+            }
+         }
+      }
+
+      // Negative Cycles make it to change even after Relaxing as the 
+      // Rotational sum will have a smaller value each time it travels around.
+      for (int j = 0; j < numEdges; j++) {
+         int from = edges[j, 0];
+         int to = edges[j, 1];
+         int weight = edges[j, 2];
+
+         if (dist[from] != Int32.MaxValue && dist[from] + weight < dist[to]) {
+            return new List<int> { -1 };
+         }
+      }
+
+      return dist.ToList ();
+   }
+}
---
+++ ShortestPath/MinCostPath.cs
@@ -0,0 +1,39 @@
+﻿public class SolutionDijkstra {
+   public int MinCost (int maxTime, int[][] edges, int[] passingFees) {
+      int N = passingFees.Length;
+      List<List<(int, int)>> AdjList = new ();
+      for (int i = 0; i < N; i++) AdjList.Add (new List<(int, int)> ());
+      foreach (var item in edges) {
+         var (from, to, weight) = (item[0], item[1], item[2]);
+         AdjList[from].Add ((to, weight));
+         AdjList[to].Add ((from, weight));
+      }
+      int[] minCost = new int[N];
+      int[] minTime = new int[N];
+      Array.Fill (minCost, int.MaxValue);
+      Array.Fill (minTime, int.MaxValue);
+
+      minCost[0] = passingFees[0];
+      minTime[0] = 0;
+
+      PriorityQueue<(int cost, int time, int node), int> pq = new PriorityQueue<(int, int, int), int> ();
+      pq.Enqueue ((passingFees[0], 0, 0), passingFees[0]);
+
+      while (pq.Count > 0) {
+         var (currCost, currTime, u) = pq.Dequeue ();
+         foreach (var (v, travelTime) in AdjList[u]) {
+            int nextTime = currTime + travelTime;
+            int nextCost = currCost + passingFees[v];
+
+            if (nextTime <= maxTime) {
+               if (nextCost < minCost[v] || nextTime < minTime[v]) {
+                  if (nextCost < minCost[v]) minCost[v] = nextCost;
+                  if (nextTime < minTime[v]) minTime[v] = nextTime;
+                  pq.Enqueue ((nextCost, nextTime, v), nextCost);
+               }
+            }
+         }
+      }
+      return (minCost[^1] == Int32.MaxValue) ? -1 : minCost[^1];
+   }
+}
