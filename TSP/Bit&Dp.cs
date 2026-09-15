82b58adda1f6ec244268cf15b5ff30179ce8304a
Sadhasivam <Sadhasivam.Saravanan@trumpf.com>
2026-09-15 19:40:03 +0530

    Travelling Salesman Problem

---
+++ TSP/Bit&Dp.cs
@@ -0,0 +1,40 @@
+﻿class ProgramTSPx2 {
+   static void MainTSPx2 (string[] args) {
+
+      const int NodeCount = 4;
+
+      int[,] costMatrix = new[,]
+      {
+          {  0, 20, 42, 25 },
+          { 20,  0, 30, 34 },
+          { 42, 30,  0, 10 },
+          { 25, 34, 10,  0 }
+      };
+
+      Dictionary<(int Node, int Mask), int> memo = [];
+      int optimalCost = TravellingSalesMan (currentNode: 0, visitedMask: 1);
+      Console.WriteLine ($"Optimal Tour Cost: {optimalCost}");
+
+      int TravellingSalesMan (int currentNode, int visitedMask) {
+         if (visitedMask == (1 << NodeCount) - 1) {
+            return costMatrix[currentNode, 0];
+         }
+
+         var state = (currentNode, visitedMask);
+         if (memo.TryGetValue (state, out int cachedScore)) {
+            return cachedScore;
+         }
+
+         int minScore = int.MaxValue;
+         for (int nextNode = 0; nextNode < NodeCount; nextNode++) {
+            if ((visitedMask & 1 << nextNode) == 0) {
+               int nextMask = visitedMask | 1 << nextNode;
+               int branchCost = TravellingSalesMan (nextNode, nextMask)
+                  + costMatrix[currentNode, nextNode];
+               minScore = Math.Min (minScore, branchCost);
+            }
+         }
+         return memo[state] = minScore;
+      }
+   }
+}
---
+++ TSP/Naive.cs
@@ -0,0 +1,30 @@
+﻿// Travelling SalesMan Problem
+class ProgramTSP {
+   static void MainTSP (string[] args) {
+
+      int Node = 4;
+      int[,] CostMat = new int[,] {
+         {0,20,42,25},
+         {20,0,30,34},
+         {42,30,0,10},
+         {25,34,10,0}
+      };
+
+      int TravellingSalesMan (int N, HashSet<int> Visited) {
+         if (Visited.Count == Node) return CostMat[N, 0];
+         int Score = int.MaxValue;
+         for (int i = 0; i < Node; i++) {
+            if (!Visited.Contains (i)) {
+               Visited.Add (i);
+               Score = Math.Min (Score, TravellingSalesMan (i, Visited) + CostMat[N, i]);
+               Visited.Remove (i);
+            }
+         }
+         return Score;
+      }
+
+      var Visited = new HashSet<int> ();
+      Visited.Add (0);
+      Console.WriteLine (TravellingSalesMan (0, Visited));
+   }
+}
