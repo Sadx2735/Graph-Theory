45c2983b683d3f839e62ebafb07f5de89b4230ce
sadhasivam <Sadhasivam.Saravanan@trumpf.com>
2026-09-17 18:29:08 +0530

    Bridges & Articulation Point

---
+++ Bridges/Br.cs
@@ -0,0 +1,41 @@
+﻿public class SolutionBridges {
+   public IList<IList<int>> CriticalConnections (int n, IList<IList<int>> connections) {
+      // Discovery time and nearest minimum back edge time.
+      int[] Time = new int[n], BackTime = new int[n];
+      // To track final results
+      IList<IList<int>> result = new List<IList<int>> ();
+      // Adjacency matrix
+      List<List<int>> Map = new List<List<int>> ();
+      for (int i = 0; i < n; i++) Map.Add ([]);
+      foreach (var item in connections) {
+         Map[item[0]].Add (item[1]);
+         Map[item[1]].Add (item[0]);
+      }
+
+      // DFS call
+      int T = 1;
+      (int, int) DFS (int Node, int Parent) {
+         // Update the Time and BackTime with the T value
+         Time[Node] = T++;
+         BackTime[Node] = Time[Node];
+         
+         // For all the neighbours check if they are not visited if not visit
+         // and track the minimum value of there exploration.
+         foreach (var nbr in Map[Node]) {
+            // Explore and get the minimum.
+            if (nbr != Parent && Time[nbr] == 0) {
+               var (DiscTime, BcTime) = DFS (nbr, Node);
+               if (Time[Node] < BcTime) result.Add (new List<int> () { Node, nbr });
+               BackTime[Node] = Math.Min (BackTime[Node], BcTime);
+            }
+            // If more than one back edge are there we need to get the minimum of them also.
+            else if (nbr != Parent && Time[nbr] != 0) {
+               BackTime[Node] = Math.Min (BackTime[Node], Time[nbr]);
+            }
+         }
+         return (Time[Node], BackTime[Node]);
+      }
+      DFS (0, -1);
+      return result;
+   }
+}
--- DFS/CyclePrint.cs
+++ DFS/CyclePrint.cs
@@ -3,20 +3,19 @@
 */
 
 // Construction of Graph.
-namespace DFSGRAPH;
-class Dummy {
-   static void RunMain () {
+namespace CycleDfs;
+class ProgramCycleDFS {
+   static void MainDFS () {
       int n = 7;
       GraphStructure Gs = new GraphStructure (n);
       Gs.AddNode (1, 0);
-      Gs.AddNode (1, 2);
-      Gs.AddNode (2, 3);
+      Gs.AddNode (2, 1);
+      Gs.AddNode (3, 2);
       Gs.AddNode (0, 4);
       Gs.AddNode (4, 3);
       Gs.AddNode (4, 5);
-      Gs.AddNode (3, 5);
       Gs.AddNode (5, 6);
-      Gs.DFS (1, new bool[n]);
+      Gs.DFS (0, -1, new int[n], new int[n]);
    }
 }
 // Graph Data Structure.
@@ -29,7 +28,7 @@ class GraphStructure {
          Map.Add (new List<int> ());
       }
    }
-   public void AddNode (int from, int to, bool BothDir = true) {
+   public void AddNode (int from, int to, bool BothDir = false) {
       Map[from].Add (to);
       if (BothDir) Map[to].Add (from);
    }
@@ -44,11 +43,21 @@ class GraphStructure {
       }
    }
    // DoDFS()
-   public void DFS (int node, bool[] visited) {
-      Console.WriteLine (node);
-      visited[node] = true;
-      foreach (var item in Map[node]) {
-         if (!visited[item]) DFS (item, visited);
+   public void DFS (int Node, int Parent, int[] visited, int[] parent) {
+      visited[Node] = 1;
+      parent[Node] = Parent;
+      foreach (var nbr in Map[Node]) {
+         if (visited[nbr] == 0) {
+            DFS (nbr, Node, visited, parent);
+         } else if (visited[nbr] == 1) {
+            int Cur = Node, Anc = nbr;
+            while (Cur != Anc) {
+               Console.Write ($"{Cur} ");
+               Cur = parent[Cur];
+            }
+            Console.Write ($"{Cur} ");
+         }
       }
+      visited[Node] = 2;
    }
 }
