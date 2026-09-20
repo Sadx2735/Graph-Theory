f0c6c725f105cb42f87f494cb51dbf176e5933e1
Sadhasivam <Sadhasivam.Saravanan@trumpf.com>
2026-09-20 23:16:13 +0530

    Tree ReRooting via DP

---
+++ ReRooting/RRDp.cs
@@ -0,0 +1,73 @@
+﻿/* DP version ReRooting.
+Sample tree:
+         0
+        / \
+       1   2
+      / \
+     3   4
+*/
+
+class ProgramRRDP {
+   static void MainRRDP (string[] args) {
+      int N = 5;
+
+      bool[] visited = new bool[N];
+      List<List<int>> AdjNode = [];
+      for (int i = 0; i < N; i++) AdjNode.Add ([]);
+
+      AdjNode[0].AddRange (new[] { 1, 2 });
+      AdjNode[1].AddRange (new[] { 0, 3, 4 });
+      AdjNode[2].Add (0);
+      AdjNode[3].Add (1);
+      AdjNode[4].Add (1);
+
+      int[] DP = new int[N];
+      int[] SB = new int[N];
+
+      for (int i = 0; i < N; i++)
+         Console.WriteLine ($"{i} : {DP[i]} , {SB[i]}");
+
+      Console.WriteLine ();
+      dfs (0); DfsPower (0, -1);
+
+      (int, int) dfs (int node) {
+         visited[node] = true;
+         int cnt = 1;
+         foreach (var nbr in AdjNode[node]) {
+            if (visited[nbr]) continue;
+            var (ct, dp) = dfs (nbr);
+            cnt += ct; DP[node] += dp;
+         }
+         SB[node] = cnt;
+         DP[node] += SB[node];
+         return (cnt, DP[node]);
+      }
+
+      void DfsPower (int node, int parent) {
+         Console.WriteLine ($"node : {node} dp : {DP[node]}");
+         foreach (var nbr in AdjNode[node]) {
+            if (nbr != parent) {
+               // from parent as root to child as root.
+               DP[node] -= DP[nbr];
+               DP[node] -= SB[nbr];
+               SB[node] -= SB[nbr];
+
+               SB[nbr] += SB[node];
+               DP[nbr] += DP[node];
+               DP[nbr] += SB[node];
+
+               DfsPower (nbr, node);
+
+               // from child as root to parent as root.
+               SB[nbr] -= SB[node];
+               DP[nbr] -= DP[node];
+               DP[nbr] -= SB[node];
+
+               DP[node] += DP[nbr];
+               DP[node] += SB[nbr];
+               SB[node] += SB[nbr];
+            }
+         }
+      }
+   }
+}
---
+++ ReRooting/RRNaive.cs
@@ -0,0 +1,54 @@
+﻿// Rerooting problems
+// Problem goes like this choose a Node make it as the Root node.
+// From that node travel to the adjacent one's. and each time you go
+// to a node the subtree becomes Score.
+// In Simple words :
+// for what node considered as root the subtree count is going to be maximum.
+// TC : O^2
+/*
+Sample tree:
+         0
+        / \
+       1   2
+      / \
+     3   4
+    / \
+   5   6
+*/
+
+class ProgramNaive {
+   static void MainNaive (string[] args) {
+      int N = 7;
+
+      bool[] visited = new bool[N];
+      List<List<int>> AdjNode = [];
+      for (int i = 0; i < N; i++) AdjNode.Add ([]);
+
+      AdjNode[0].AddRange (new[] { 1, 2 });
+      AdjNode[1].AddRange (new[] { 0, 3, 4 });
+      AdjNode[2].Add (0);
+      AdjNode[3].AddRange (new[] { 1, 5, 6 });
+      AdjNode[4].Add (1);
+      AdjNode[5].Add (3);
+      AdjNode[6].Add (3);
+
+      int total = 0;
+      for (int nde = 0; nde < N; nde++) {
+         total = 0;
+         visited = new bool[N];
+         dfs (nde);
+         Console.WriteLine ($"Score when choosing start as {nde} is {total}");
+      }
+
+      int dfs (int node) {
+         visited[node] = true;
+         int cnt = 1;
+         foreach (var nbr in AdjNode[node]) {
+            if (visited[nbr]) continue;
+            cnt += dfs (nbr);
+         }
+         total += cnt;
+         return cnt;
+      }
+   }
+}
